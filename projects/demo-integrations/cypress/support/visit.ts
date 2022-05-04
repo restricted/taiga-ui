3d3fce8e46e6aba11096958c13acd6d3eb63e1a1
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-05-04 12:26:54 +0300

    feat(testing): new `tuiWaitAllIconsInside` cypress-command

--- projects/demo-integrations/cypress/support/visit.ts
+++ projects/demo-integrations/cypress/support/visit.ts
@@ -36,7 +36,7 @@ const setBeforeLoadOptions = (
 export function tuiVisit(path: string, options: TuiVisitOptions = {}): void {
     const {
         inIframe = true,
-        waitAllIcons = true,
+        waitAllIcons = false,
         enableNightMode = false,
         hideCursor = true,
         hideScrollbar = true,
