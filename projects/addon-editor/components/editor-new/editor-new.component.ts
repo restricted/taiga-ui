e01a0012ea7e9ecea8b4287f0109612ac1be98a4
splincode <splincodewd@yandex.ru>
2022-05-16 15:18:02 +0300

    fix(addon-editor): support for editing links in the new editor

--- projects/addon-editor/components/editor-new/editor-new.component.ts
+++ projects/addon-editor/components/editor-new/editor-new.component.ts
@@ -1,3 +1,4 @@
+import {DOCUMENT} from '@angular/common';
 import {
     ChangeDetectionStrategy,
     ChangeDetectorRef,
@@ -21,7 +22,6 @@ import {TuiEditorTool} from '@taiga-ui/addon-editor/enums';
 import {TIPTAP_EDITOR} from '@taiga-ui/addon-editor/tokens';
 import {
     AbstractTuiControl,
-    ALWAYS_FALSE_HANDLER,
     isNativeFocusedIn,
     TUI_FOCUSABLE_ITEM_ACCESSOR,
     TuiBooleanHandler,
@@ -33,6 +33,7 @@ import {Observable} from 'rxjs';
 
 import {TUI_EDITOR_NEW_PROVIDERS} from './editor-new.providers';
 
+// @dynamic
 @Component({
     selector: 'tui-editor[new]',
     templateUrl: './editor-new.component.html',
@@ -53,6 +54,9 @@ export class TuiEditorNewComponent
     @ViewChild(TuiEditLinkComponent, {read: ElementRef})
     private readonly editLink?: ElementRef<HTMLElement>;
 
+    @ViewChild(TuiEditLinkComponent)
+    private readonly editLinkRef?: TuiEditLinkComponent;
+
     @Input()
     @tuiDefaultProp()
     exampleText = '';
@@ -72,12 +76,16 @@ export class TuiEditorNewComponent
         @Inject(ChangeDetectorRef) changeDetectorRef: ChangeDetectorRef,
         @Inject(TIPTAP_EDITOR) readonly editorLoaded$: Observable<Editor | null>,
         @Inject(TuiTiptapEditorService) readonly editorService: TuiEditor,
+        @Inject(DOCUMENT)
+        private readonly documentRef: Document,
     ) {
         super(control, changeDetectorRef);
     }
 
     get dropdownSelectionHandler(): TuiBooleanHandler<Range> {
-        return this.focused ? this.isSelectionLink : ALWAYS_FALSE_HANDLER;
+        return this.currentFocusedNodeIsAnchor()
+            ? () => this.isValidSelectionAnchorRange()
+            : () => this.previousFocusedNodeIsAnchor() || this.focusedNodeIsEditorLink();
     }
 
     get editor(): TuiEditor | null {
@@ -100,6 +108,10 @@ export class TuiEditorNewComponent
         return (
             !!this.exampleText && this.computedFocused && !this.hasValue && !this.readOnly
         );
+    }
+
+    private get selection(): Selection | null {
+        return this.documentRef.getSelection();
     }
 
     onHovered(hovered: boolean): void {
@@ -137,7 +149,35 @@ export class TuiEditorNewComponent
         return '';
     }
 
-    private readonly isSelectionLink = (): boolean => !!this.editor?.isActive('link');
+    private currentFocusedNodeIsAnchor(): boolean {
+        return !!this.selection?.anchorNode?.parentElement?.closest('a');
+    }
+
+    private previousFocusedNodeIsAnchor(): boolean {
+        return !!this.selection?.focusNode?.parentElement?.closest('a');
+    }
+
+    private focusedNodeIsEditorLink(): boolean {
+        return !!this.editLinkRef?.focused();
+    }
+
+    private isValidSelectionAnchorRange(): boolean {
+        let isValid = false;
+        const isAnchor = this.currentFocusedNodeIsAnchor();
+
+        if (isAnchor && this.selection) {
+            const range = this.selection.getRangeAt(0);
+            const delta = Math.abs(range.endOffset - range.startOffset);
+            const length = (range.commonAncestorContainer as Text).length || 0;
+
+            isValid =
+                delta > 0 ||
+                (length === 1 && range.startOffset === 0) ||
+                (length > 1 && range.endOffset < length);
+        }
+
+        return isValid;
+    }
 
     private get hasValue(): boolean {
         return !!this.value;
